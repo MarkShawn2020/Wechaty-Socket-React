c3c7998fa5b781574c9215484279a7108182ea75
MarkShawn2020 <877210964@qq.com>
2020-09-26 11:11:03 +0800

    rm extra

--- frontend_next/src/store/ActionTypes.ts
+++
@@ -1,5 +0,0 @@
-export enum ActionTypes {
-  updateUserStatus,
-  updateUserStatusLogined,
-  resetUserStatus,
-}
--- frontend_next/src/utils/$http.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-if (!process.env.REACT_APP_BACKEND_SERVER) throw new Error("not found base url")
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_SERVER
-axios.defaults.timeout = 5000 // 设置这么短之后调试就要注意了，都会超时的
-
-const $http = axios
-export { $http }
-export default axios
